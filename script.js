f3bd07eb75782c3f3b135147c4ac3776c0a06049
Aadhiraj sunar <ptgame75@gmail.com>
2026-02-11 00:29:37 +0545

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showBhawana() {
-    var section = document.getElementById("messageSection");
-    section.style.display = "block";
-    // क्लिक गरेपछि बिस्तारै तल सार्ने (Scroll)
-    window.scrollTo({ top: section.offsetTop, behavior: 'smooth' });
-}
