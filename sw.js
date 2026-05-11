71c3267e0ee2de792b24828f8668aef632ced5d5
Aadhiraj sunar <ptgame75@gmail.com>
2026-05-11 23:34:17 +0545

    Implement asset caching in service worker
    Add service worker caching for assets during installation.

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
+const cacheName = 'adhiraj-v1';
+const assets = [
+  './',
+  './index.html',
+  './home.html',
+  './4870.png',
+  './a.0063.jpg'
+];
+
+// फाइलहरू सेभ गर्ने
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  // पहिला नेटवर्कबाट डाटा तान्ने, छैन भने मात्र क्यास हेर्ने
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
