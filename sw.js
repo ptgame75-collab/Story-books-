c9fa727499d0cb3d4e2c1805f6518e40ccb60ec0
Aadhiraj sunar <ptgame75@gmail.com>
2026-06-29 01:05:07 +0545

    Implement service worker for caching requests

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'diary-v2';
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) return cachedResponse;
+            return fetch(event.request).then((networkResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
+});
